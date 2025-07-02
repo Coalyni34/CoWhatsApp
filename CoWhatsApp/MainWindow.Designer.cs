62b458b60b0d7a583112e291fa547d41dd9b5dbc
coalyni <coalyni@WIN-6BTF8LOLCH8>
2025-07-02 21:40:04 +0300

    Добавьте файлы проекта.

---
+++ CoWhatsApp/MainWindow.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace CoWhatsApp
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
+            this.cowBrowser = new CefSharp.WinForms.ChromiumWebBrowser();
+            this.SuspendLayout();
+            // 
+            // cowBrowser
+            // 
+            this.cowBrowser.ActivateBrowserOnCreation = false;
+            this.cowBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cowBrowser.Location = new System.Drawing.Point(0, 0);
+            this.cowBrowser.Name = "cowBrowser";
+            this.cowBrowser.Size = new System.Drawing.Size(800, 450);
+            this.cowBrowser.TabIndex = 0;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cowBrowser);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainWindow";
+            this.Text = "CoWhatsApp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private CefSharp.WinForms.ChromiumWebBrowser cowBrowser;
+    }
+}
+
---
+++ CoWhatsApp/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoWhatsApp
+{
+    public partial class MainWindow : Form
+    {
+        private readonly string cowUrl = "https://web.whatsapp.com/";
+        public MainWindow()
+        {
+            InitializeComponent();
+            cowBrowser.Load(cowUrl);
+        }
+    }
+}
